15fefb46aa408f601bd9762fc44b7a513e731c1b
Yeppman <yeppman13@gmail.com>
2020-10-09 12:00:41 +0100

    made changes

--- Readme.txt
+++ Readme.txt
@@ -4,3 +4,5 @@ Template Name: Gp
 Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
 Author: BootstrapMade.com
 License: https://bootstrapmade.com/license/
+
+b70f85210cabdef9b64d6fdf6e998b046c40484d
--- assets/js/main.js
+++ assets/js/main.js
@@ -167,6 +167,23 @@
     }
   });
 
+  $(".news-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      768: {
+        items: 2
+      },
+      900: {
+        items: 2
+      }
+    }
+  });
+
   // Porfolio isotope and filter
   $(window).on('load', function() {
     var portfolioIsotope = $('.portfolio-container').isotope({
